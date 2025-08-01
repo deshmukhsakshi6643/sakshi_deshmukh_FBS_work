bc6df54bfd0086012a08e9bdc80b572ecfd014f7
Deshmukh Sakshi <113850441+deshmukhsakshi6643@users.noreply.github.com>
2025-08-01 21:01:35 +0530

    Delete simpleIntrest.py

--- simpleIntrest.py
+++
@@ -1,7 +0,0 @@
-P = int (input("Enter principle amount:"))
-R = int (input("Enter rate:"))
-T = int (input("Enter time(yeras):"))
-
-simple_int = (P * T * R )/100
-print(f'Simple_intrest:{simple_int}')
-
